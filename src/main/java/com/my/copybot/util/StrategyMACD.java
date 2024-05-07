3d0016381b47cc2e279327edf4afa8ea9277b696
4rad95 <4rad@i.ua>
2024-05-07 21:35:23 +0200

    ReMade Strategy ver.1.006

--- src/main/java/com/my/copybot/util/StrategyMACD.java
+++ src/main/java/com/my/copybot/util/StrategyMACD.java
@@ -28,8 +28,8 @@ public class StrategyMACD {
         EMAIndicator sma24 = new EMAIndicator(closePrice, 100);
         EMAIndicator emaShort = new EMAIndicator(closePrice, 5);
         EMAIndicator emaLong = new EMAIndicator(closePrice, 15);
-        MACDIndicator macd = new MACDIndicator(closePrice, 12, 26);
-        EMAIndicator emaMacd = new EMAIndicator(macd, 9);
+        MACDIndicator macd = new MACDIndicator(closePrice, 10, 30);
+        EMAIndicator emaMacd = new EMAIndicator(macd, 20);
 
         RSIIndicator rsi = new RSIIndicator(closePrice, 14);
 
@@ -102,8 +102,8 @@ public class StrategyMACD {
         EMAIndicator sma24 = new EMAIndicator(closePrice, 100);
         EMAIndicator emaShort = new EMAIndicator(closePrice, 5);
         EMAIndicator emaLong = new EMAIndicator(closePrice, 15);
-        MACDIndicator macd = new MACDIndicator(closePrice, 12, 26);
-        EMAIndicator emaMacd = new EMAIndicator(macd, 9);
+        MACDIndicator macd = new MACDIndicator(closePrice, 10, 30);
+        EMAIndicator emaMacd = new EMAIndicator(macd, 20);
 
 
         RSIIndicator rsi = new RSIIndicator(closePrice, 14);
