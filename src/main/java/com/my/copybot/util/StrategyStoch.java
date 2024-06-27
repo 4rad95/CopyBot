f58408dd86e3a813204e8d4ffd3478265794ef06
4rad95 <4rad@i.ua>
2024-06-27 22:16:08 +0200

    Update strategy

--- src/main/java/com/my/copybot/util/StrategyStoch.java
+++ src/main/java/com/my/copybot/util/StrategyStoch.java
@@ -48,7 +48,7 @@ public class StrategyStoch {
                 .and(new UnderIndicatorRule(rsi, deltaK));
 
         deltaK = Decimal.valueOf(102);
-        if (smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() < smoothedStochRsi.getValue(maxIndex - 3).multipliedBy(100).intValue()) {
+        if (smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() > smoothedStochRsi.getValue(maxIndex - 3).multipliedBy(100).intValue()) {
             deltaK = Decimal.valueOf(-2);
             //         System.out.println("SHORT " +series.getName() + "   StochRSI %K at index : " + smoothedStochRsi.getValue(series.getBarCount()-1).multipliedBy(100) + "   StochRSI %D at index : " + stochRsiD.getValue(series.getBarCount()-1).multipliedBy(100));
         }
@@ -79,7 +79,7 @@ public class StrategyStoch {
         int maxIndex = series.getEndIndex();
         Decimal deltaK = Decimal.valueOf(-2);
 
-        if ((stochRsiD.getValue(maxIndex).multipliedBy(100).intValue() < smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue())
+        if ((stochRsiD.getValue(maxIndex).multipliedBy(100).intValue() > smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue())
                 && smoothedStochRsi.getValue(maxIndex - 1).compareTo(stochRsiD.getValue(maxIndex - 2)) > 0
                 && smoothedStochRsi.getValue(maxIndex).compareTo(smoothedStochRsi.getValue(maxIndex - 1)) < 0
                 && macd.getValue(maxIndex).compareTo(macd.getValue(maxIndex - 1)) < 0
@@ -91,7 +91,7 @@ public class StrategyStoch {
                 .and(new UnderIndicatorRule(rsi, deltaK));
 
         deltaK = Decimal.valueOf(120);
-        if (smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() > smoothedStochRsi.getValue(maxIndex - 3).multipliedBy(100).intValue()) {
+        if (smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() > smoothedStochRsi.getValue(maxIndex - 2).multipliedBy(100).intValue()) {
             deltaK = Decimal.valueOf(-20);
             //         System.out.println("SHORT "+series.getName()+ "   rsi = " + rsi.getValue(series.getBarCount()-1)); //+"   StochRSI %K at index : " + smoothedStochRsi.getValue(series.getBarCount()-1).multipliedBy(100) + "   StochRSI %D at index : " + stochRsiD.getValue(series.getBarCount()-1).multipliedBy(100));
         }
