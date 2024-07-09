a2041e64dd9ea5d2544bbd055cbd92c82f0f3113
4rad95 <4rad@i.ua>
2024-07-10 00:53:05 +0200

    Update StrategyUsing

--- src/main/java/com/my/copybot/util/StrategyStoch.java
+++ src/main/java/com/my/copybot/util/StrategyStoch.java
@@ -153,8 +153,8 @@ public class StrategyStoch {
         int maxIndex = series.getEndIndex();
 
 
-        return smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() > smoothedStochRsi.getValue(maxIndex - 2).multipliedBy(100).intValue()
-                //    || smoothedStochRsi.getValue(maxIndex - 1).multipliedBy(100).intValue() > stochRsiD.getValue(maxIndex - 1).multipliedBy(100).intValue()
+        return //smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() > smoothedStochRsi.getValue(maxIndex - 2).multipliedBy(100).intValue()
+                smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() > stochRsiD.getValue(maxIndex).multipliedBy(100).intValue()
                 //  || stochRsiD.getValue(maxIndex - 1).multipliedBy(100).intValue() < stochRsiD.getValue(maxIndex).multipliedBy(100).intValue();
                 ;
     }
@@ -201,8 +201,8 @@ public class StrategyStoch {
 
         int maxIndex = series.getEndIndex();
 
-        return smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() < smoothedStochRsi.getValue(maxIndex - 2).multipliedBy(100).intValue()
-                //          || smoothedStochRsi.getValue(maxIndex - 1).multipliedBy(100).intValue() < stochRsiD.getValue(maxIndex - 1).multipliedBy(100).intValue()
+        return // smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() < smoothedStochRsi.getValue(maxIndex - 2).multipliedBy(100).intValue()
+                smoothedStochRsi.getValue(maxIndex).multipliedBy(100).intValue() < stochRsiD.getValue(maxIndex).multipliedBy(100).intValue()
                 //     || stochRsiD.getValue(maxIndex - 1).multipliedBy(100).intValue() > stochRsiD.getValue(maxIndex).multipliedBy(100).intValue()
                 ;
     }
